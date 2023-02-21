d326853f44a1cc5e3c4bfd961f7b5a5587219a01
shuja-shah <shujashah786@gmail.com>
2023-02-21 20:28:01 +0500

    Created Table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id bigint NOT NULL,
+    name varchar(100) NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts integer NOT NULL,
+    neutered boolean NOT NULL,
+    weight_kg double precision NOT NULL
 );
