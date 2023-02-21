09db138960797aa0337ec59d8b468a38a1298f7d
shuja-shah <shujashah786@gmail.com>
2023-02-21 20:54:25 +0500

    Refactor Schema

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id bigint NOT NULL,
-    name varchar(100) NOT NULL,
-    date_of_birth date NOT NULL,
-    escape_attempts integer NOT NULL,
-    neutered boolean NOT NULL,
-    weight_kg double precision NOT NULL
+    id SERIAL NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INTEGER NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL NOT NULL
 );
