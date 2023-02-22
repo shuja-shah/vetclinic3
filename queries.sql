79e9478cf110299ed41aff209dd64f1f661fb2a6
shuja-shah <shujashah786@gmail.com>
2023-02-22 21:05:19 +0500

    Correctly put Data in Quires instead of Schema

--- queries.sql
+++ queries.sql
@@ -68,3 +68,88 @@ WHERE
     weight_kg BETWEEN 10.4
     AND 17.3;
 
+/* First Transaction Update */
+BEGIN;
+
+UPDATE
+    animals
+SET
+    species = 'unspecified';
+
+select
+    *
+from
+    animals;
+
+ROLLBACK;
+
+select
+    *
+from
+    animals;
+
+/* Second Transaction Update */
+BEGIN;
+
+UPDATE
+    animals
+SET
+    species = 'digimon'
+WHERE
+    name LIKE '%mon';
+
+UPDATE
+    animals
+SET
+    species = 'pokemon'
+WHERE
+    species IS NULL;
+
+COMMIT;
+
+/* Third Transaction Update */
+BEGIN;
+
+DELETE FROM
+    animals;
+
+SELECT
+    *
+FROM
+    animals;
+
+ROLLBACK;
+
+SELECT
+    *
+FROM
+    animals;
+
+/* Fourth Transaction Update */
+BEGIN;
+
+DELETE FROM
+    animals
+WHERE
+    date_of_birth > '2022-01-01';
+
+SAVEPOINT delete_after_2022;
+
+UPDATE
+    animals
+SET
+    weight_kg = weight_kg * -1;
+
+ROLLBACK TO delete_after_2022;
+
+UPDATE
+    animals
+SET
+    weight_kg = weight_kg * -1
+WHERE
+    weight_kg < 0;
+
+COMMIT;
+
+
+/* MIlestone 2 Questions */
